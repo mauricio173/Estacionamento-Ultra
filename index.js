e913ce1f9bcd22969fdb55c842cfc7c43fa11227
Mauricio Marques <101911920+mauricio173@users.noreply.github.com>
2024-12-05 01:16:00 -0300

    update index.js

--- index.js
+++ index.js
@@ -3,27 +3,8 @@ const cpf = document.querySelector("#cpf");
 const dataInicio = document.querySelector("#dataInicio");
 const dataFinal = document.querySelector("#dataFinal");
 
-function dataHorario() {
-  const dataHoraAtual = new Date();
-  const ano = dataHoraAtual.getFullYear();
-  const mes = String(dataHoraAtual.getMonth() + 1).padStart(2, "0");
-  const dia = String(dataHoraAtual.getDate()).padStart(2, "0");
-  const horas = String(dataHoraAtual.getHours()).padStart(2, "0");
-  const minutos = String(dataHoraAtual.getMinutes()).padStart(2, "0");
-  const segundos = String(dataHoraAtual.getSeconds()).padStart(2, "0");
-  const milisegundos = String(dataHoraAtual.getMilliseconds()).padStart(2, "0");
-  const data = `${ano}-${mes}-${dia}`;
-  const horario = `${horas}:${minutos}:${segundos}:${milisegundos}`;
-  return {
-  data: data,
-  horario: horario
-  };
-}
-dataHorario();
-const dataData = dataHorario.data;
-const dataHora = dataHorario.horario;
-const hoje = new Date();
-const opcoes = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', timezone: 'UTC' };
-console.log(hoje.toLocaleDateString('pt-BR', opcoes));
 
-console.log(hoje.getFullYear() + "-" + (hoje.getMonth()+1) + "-" + (hoje.getDate() < 10 ? "0" + hoje.getDate() : hoje.getDate()));
+const hoje = new Date();
+const dataAtual = hoje.getFullYear() + "-" + (hoje.getMonth()+1) + "-" + (hoje.getDate() < 10 ? "0" + hoje.getDate() : hoje.getDate());
+console.log(dataAtual);
+// console.log(hoje.getFullYear() + "-" + (hoje.getMonth()+1) + "-" + (hoje.getDate() < 10 ? "0" + hoje.getDate() : hoje.getDate()));
