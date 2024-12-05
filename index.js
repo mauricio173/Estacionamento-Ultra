2d0057895622c2e07f6e95ba41f59e0c3e54361c
Mauricio Marques <101911920+mauricio173@users.noreply.github.com>
2024-12-05 01:00:50 -0300

    update index.js

--- index.js
+++ index.js
@@ -21,14 +21,6 @@ function dataHorario() {
 }
 const dataData = dataHorario.data;
 const dataHora = dataHorario.horario;
-let fim = "2024-05-12";
-let inicio = "2024-05-11";
-function difDias(fim, inicio) {
-let fimItem = fim;
-let inicioItem = inicio;
-const DiffInMs = new Date(fimItem) - new Date(inicioItem);
-const DiffInDays = DiffInMs / (1000 * 60 * 60 * 24);
-console.log(DiffInDays);
-}
-difDias(fim, inicio);
-console.log(difDias(fim, inicio));
+
+console.log(dataData);
+console.log(dataHora);
