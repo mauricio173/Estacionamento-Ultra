09a50827dca59715a948cc9f525fb8c5c8df1f12
Mauricio Marques <101911920+mauricio173@users.noreply.github.com>
2024-12-05 00:54:41 -0300

    update index.js

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-console.log("oi");
+const nome = document.querySelector("#nome");
+const cpf = document.querySelector("#cpf");
+const dataInicio = document.querySelector("#dataInicio");
+const dataFinal = document.querySelector("#dataFinal");
