71f5877351bf0996f2c32807311266d00b8482e2
Mauricio Marques <101911920+mauricio173@users.noreply.github.com>
2024-12-05 01:30:39 -0300

    update index.js

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const dataInicio = document.querySelector("#dataInicio");
 const dataFinal = document.querySelector("#dataFinal");
 const hoje = new Date();
 const dataAtual = hoje.getFullYear() + "-" + (hoje.getMonth()+1) + "-" + (hoje.getDate() < 10 ? "0" + hoje.getDate() : hoje.getDate());
-// const dataFutura = hoje.getFullYear() + "-" + (hoje.getMonth()+7) + "-" + (hoje.getDate() < 10 ? "0" + hoje.getDate() : hoje.getDate());
 
 // Criar uma nova data baseada na atual
 const dataFutura = new Date(hoje);
