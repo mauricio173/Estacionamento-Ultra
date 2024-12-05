a7ef6e44d00fe0457c346af94762cad51c88dff5
Mauricio Marques <101911920+mauricio173@users.noreply.github.com>
2024-12-05 01:32:13 -0300

    update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const nome = document.querySelector("#nome");
 const cpf = document.querySelector("#cpf");
 const dataInicio = document.querySelector("#dataInicio");
 const dataFinal = document.querySelector("#dataFinal");
+const cadastrar = document.querySelector("#cadastrar");
 const hoje = new Date();
 const dataAtual = hoje.getFullYear() + "-" + (hoje.getMonth()+1) + "-" + (hoje.getDate() < 10 ? "0" + hoje.getDate() : hoje.getDate());
 
