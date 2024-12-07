a4ead5108349e42b4f51b0c9123f3d87ab55a52f
Mauricio Marques <101911920+mauricio173@users.noreply.github.com>
2024-12-07 01:33:38 -0300

    update Versionamento/index.js

--- Versionamento/index.js
+++ Versionamento/index.js
@@ -144,7 +144,7 @@ function mostrarResultado(mensagem, valido) {
 
  // Adiciona um novo CPF válido na lista
  console.log(nomeValue.startsWith(" "));
- if (valido && nome.value.trim() !== "" && nomeValue.startsWith(" ")) {
+ if (valido && nome.value.trim() !== "" && nome.value.startsWith(" ")) {
   const invalido = document.querySelector("#invalido");
   invalido.style = "display: none";
   const el = document.createElement("p");
