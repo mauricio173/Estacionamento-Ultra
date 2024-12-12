8ad03f8feb071b99106a509d47d9737e4b140f65
Mauricio Marques <101911920+mauricio173@users.noreply.github.com>
2024-12-12 02:13:59 -0300

    update Versionamento/index.js

--- Versionamento/index.js
+++ Versionamento/index.js
@@ -265,7 +265,7 @@ console.log(resultados);
     invalido.innerHTML = "CPF já inserido.";
    }
 
-   if (itemTexto.includes(nomeValue, 12)) {
+   if (itemTexto.includes(nomeValue, 13)) {
     console.log(nomeValue);
     invalido.style = "display: block; color: black";
     invalido.innerHTML = "Nome já inserido.";
